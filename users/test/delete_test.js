79b8b96023c6983a98faf93e7151482d722f76fb
Erik S Carlsten <ecarlste@gmail.com>
2017-05-19 14:35:01 -0600

    Added an integration test that checks removing using a User instance

---
+++ users/test/delete_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const User = require('../src/user')
+const it = require('mocha').it
+const beforeEach = require('mocha').beforeEach
+const describe = require('mocha').describe
+
+describe('Deleting a user', () => {
+  let joe
+
+  beforeEach((done) => {
+    joe = new User({name: 'Joe'})
+    joe.save()
+      .then(() => done())
+  })
+
+  it('model instance remove', (done) => {
+    joe.remove()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user === null)
+        done()
+      })
+  })
+})
