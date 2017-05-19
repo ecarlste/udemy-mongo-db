d878af3c94f2c3a4fcc6b73aff1195b6d99e7bc0
Erik S Carlsten <ecarlste@gmail.com>
2017-05-19 14:25:27 -0600

    Added integration test for finding a user by id

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -8,13 +8,13 @@ describe('Reading users out of the database', () => {
   let joe
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' })
+    joe = new User({name: 'Joe'})
     joe.save()
       .then(() => done())
   })
 
   it('finds all users with a name of joe', (done) => {
-    User.find({ name: 'Joe' })
+    User.find({name: 'Joe'})
       .then((users) => {
         assert(users[0]._id.toString() === joe._id.toString())
         done()
@@ -22,6 +22,10 @@ describe('Reading users out of the database', () => {
   })
 
   it('find a user with a particular id', (done) => {
-
+    User.findOne({_id: joe._id})
+      .then((user) => {
+        assert(user.name === 'Joe')
+        done()
+      })
   })
 })
