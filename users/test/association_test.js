fa9a2faf2e43f0f32a0dae30ea6c79c731eac491
Erik S Carlsten <ecarlste@gmail.com>
2017-05-31 09:59:00 -0600

    Added a test to ensure that relations between users and comments saved

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -31,4 +31,27 @@ describe('Associations', () => {
         done()
       })
   })
+
+  it('saves a full relation graph from user to comment', (done) => {
+    User.findOne({name: 'Joe'})
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe')
+        assert(user.blogPosts[0].title === 'JS is Great')
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on a great post')
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe')
+
+        done()
+      })
+  })
 })
