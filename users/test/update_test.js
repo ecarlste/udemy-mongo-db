857d2c2b24c8af435b1416ff7b81026c4db64468
Erik S Carlsten <ecarlste@gmail.com>
2017-05-30 21:24:22 -0600

    Removed the postCount field and made it a getter method

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -43,13 +43,4 @@ describe('Updating records', () => {
   it('A model class can find a record with an Id and update', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done)
   })
-
-  it('A user can having their postCount incremented by 1', (done) => {
-    User.update({name: 'Joe'}, {$inc: {postCount: 1}})
-      .then(() => User.findOne({name: 'Joe'}))
-      .then((user) => {
-        assert(user.postCount === 1)
-        done()
-      })
-  })
 })
