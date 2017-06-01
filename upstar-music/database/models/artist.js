b6c8d6a9ebc2a09bcfa0bfca72ce3af416cb2e24
Erik S Carlsten <ecarlste@gmail.com>
2017-06-01 08:54:47 -0600

    Added initial definition of the Artist schema and model

--- upstar-music/database/models/artist.js
+++ upstar-music/database/models/artist.js
@@ -1,1 +1,23 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [{
+        type: Schema.Types.ObjectId,
+        ref: 'album'
+    }]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
