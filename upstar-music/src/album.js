92510251349596c74fb707ee08db17dd9727bca4
Erik S Carlsten <ecarlste@gmail.com>
2017-06-01 08:46:25 -0600

    Added first version of the Album schema and model

---
+++ upstar-music/src/album.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    title: String,
+    date: Date,
+    copiesSold: Number,
+    numberTracks: Number,
+    image: String,
+    revenue: Number
+});
+
+const Album = mongoose.model('album', AlbumSchema);
+
+module.exports = Album;
